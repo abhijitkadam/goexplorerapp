cedbf821661892671fecf4ddd104ae2b27efcb88
AbhijitKadam <abhijitk100@gmail.com>
2021-08-16 12:40:45 +0530

    added goconcurrent go routines with waitgroup and also with defer

--- main.go
+++ main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
 	//GoKeyWordDemo()
+	ConcurrenyWithWaitGroupExampleWithDefer()
+}
+
+func ConcurrenyWithWaitGroupExample() {
+	var wg sync.WaitGroup
 
 	for i := 0; i < 10000; i++ {
+		wg.Add(1)
 		go func(count int) {
+			fmt.Println("Running concurrently : ", count)
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("Completed!")
+}
+
+func ConcurrenyWithWaitGroupExampleWithDefer() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		go func(count int) {
+			//With defer we made sure that wg.Done() would always be called when this func returns
+			//So we are defering execution of wg.Done untill the end i.e. when function returns
+			defer wg.Done()
 			fmt.Println("Running concurrently : ", count)
 		}(i)
 	}
 
-	time.Sleep(time.Second * 2)
+	wg.Wait()
+	fmt.Println("Completed!")
 }
