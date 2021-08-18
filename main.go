8109a59f08e4c2d29f978a73261e6c33ad771bad
AbhijitKadam <abhijitk100@gmail.com>
2021-08-18 12:45:00 +0530

    methods on hotels and filters on hotel slice with HOF

--- main.go
+++ main.go
@@ -10,6 +10,49 @@ import "fmt"
 //The solution is to use pointer. So empty value of pointer will be nil.
 
 func main() {
+	hotels, err := FetchHotelsFromUrlUsingJsonDecoder("http://localhost:3000/inventory")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fhotels := Hotels(hotels).FilterHotelByRoomType("LARGE")
+	fmt.Println(fhotels)
+
+	shotels := Hotels(hotels).FilterByFn(func(h *Hotel) bool {
+		return h.RoomType == "SMALL"
+	})
+
+	fmt.Println(shotels)
+
+	filterFn := func(h *Hotel) bool {
+		return h.RoomType == "X-LARGE"
+	}
+
+	xlhotels := Hotels(hotels).FilterBy(filterFn)
+	fmt.Println(xlhotels)
+
+}
+
+func MethodDemo() {
+	hotel := NewHotel("gold", "HOTEL-DDF", "SMALL", 20.0)
+
+	p := hotel.GetPrice()
+	fmt.Println(p)
+
+	//if use the func instead of method
+	//SetPrice(hotel, 55)
+
+	hotel.SetPrice(50)
+	fmt.Println("New Price: ", hotel.GetPrice())
+
+	//SetHotelPrice(&hotel, 65)
+
+	hotel.SetHotelPrice(60)
+	fmt.Println("After SetHotelPrice: ", hotel.GetPrice())
+}
+
+func SlicesDemo() {
 	//GoSlices()
 	//GoSlicesRange()
 
