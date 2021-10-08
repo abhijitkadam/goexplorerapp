9db3aa947c9d4a77c39256dad4dade28e361516e
AbhijitKadam <abhijitk100@gmail.com>
2021-10-08 17:54:21 +0530

    echo apis exploration

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module explorerapp
 go 1.16
 
 require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo/v4 v4.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.8.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"strings"
 
+	"github.com/labstack/echo"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
@@ -16,14 +16,88 @@ func HomeHandler(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(rw, "This is go server")
 }
 
+func EchoHandler(c echo.Context) error {
+	//1. Request
+	//Binding is there: https://echo.labstack.com/guide/binding/
+	//However if need be can ge the request like this:
+	//c.Request()
+
+	//2. ResponseWriter
+	//There are helper methods
+	//c.String(...)
+	//c.JSON(..)
+	//However if need be can ge the response writer
+	//c.Response().Header().Add(..)
+	//c.Response().WriteHeader(...)
+	//fmt.Fprint(c.Response().Writer, "write it")
+
+	//3 Context
+	//c.Set("userinfo", user)
+	//user, ok := c.Get("userinfo").(*User)
+
+	//4. http.Error()
+	//For echo in case of error there is a helper
+	//return echo.NewHTTPError(http.StatusBadRequest, "Please provide valid input")
+	//return echo.NewHTTPError(http.StatusUnauthorized, "Please provide valid credentials")
+
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+///hotels/:name
+
+type HotelCode struct {
+	ID string `param:"id" query:"id" header:"id" form:"id" json:"id" xml:"id"`
+}
+
+// User
+type User struct {
+	Name  string `json:"name" form:"name" query:"name"`
+	Email string `json:"email" form:"email" query:"email"`
+}
+
 func main() {
 	Config()
 
-	http.HandleFunc("/home", HomeHandler)
+	e := echo.New()
 
-	fmt.Println("Starting server : ", viper.Get("SERVER"), " on port ", viper.GetInt("port"))
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			fmt.Println(c.Request().URL)
+			return next(c)
+		}
+	})
 
-	log.Fatal(http.ListenAndServe(":"+viper.GetString("port"), nil))
+	e.POST("/users", func(c echo.Context) (err error) {
+		u := new(User)
+		if err = c.Bind(u); err != nil {
+			return
+		}
+
+		return c.JSON(http.StatusOK, u)
+	})
+
+	e.GET("/users", func(c echo.Context) (err error) {
+		u := new(User)
+		if err = c.Bind(u); err != nil {
+			return
+		}
+
+		return c.JSON(http.StatusOK, u)
+	})
+
+	e.GET("/hotels/:id", func(c echo.Context) (err error) {
+		//hc := new(HotelCode)
+		// if err = c.Bind(hc); err != nil {
+		// 	return
+		// }
+
+		code := c.Param("id")
+
+		return c.JSON(http.StatusOK, code)
+	})
+
+	e.GET("/", EchoHandler)
+	e.Logger.Fatal(e.Start(":" + viper.GetString("port")))
 
 }
 
